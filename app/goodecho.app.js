8b918aeece6d4d9a41be6d0e23440297e8ac33fc
mohsen-001 <115645631+mohsen-001@users.noreply.github.com>
2022-10-13 14:58:04 +0430

    changes just added

---
+++ app/goodecho.app.js
@@ -0,0 +1,28 @@
+const g_btn_loader = document.querySelector('[data-btn-loader]');
+// console.log('hey');
+
+function btnLoader(btnPlace){
+    const loader = document.createElement('lottie-player');
+    loader.setAttribute('src', './loader/g-btn-loader.json');
+    loader.setAttribute('background', 'transparent');
+    loader.setAttribute('speed', '1');
+    loader.setAttribute('style', 'width: 50px');
+    // loader.setAttribute('height', '70px');
+    loader.setAttribute('loop', '');
+    loader.setAttribute('autoplay', '');
+    btnPlace.appendChild(loader);
+}
+
+g_btn_loader.addEventListener('click', () => {
+    const btn_width = g_btn_loader.offsetWidth;
+    const btn_height = g_btn_loader.offsetHeight;
+    g_btn_loader.innerHTML = '';
+    g_btn_loader.style.width = `${btn_width}px`;
+    g_btn_loader.style.height = `${btn_height}px`;
+    g_btn_loader.style.display = 'flex';
+    g_btn_loader.style.justifyContent = 'center';
+    g_btn_loader.style.alignItem = 'center';
+    btnLoader(g_btn_loader);
+})
+
+/* <lottie-player src="./loader/btn-loader.json"  background="transparent"  speed="1.5"  style="width: 70px; height: 70px;"  loop autoplay></lottie-player> */
