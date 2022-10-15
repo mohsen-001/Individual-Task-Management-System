981a67daa77b3d40c12b66b81cb231ed76ae9d37
mohsen-001 <115645631+mohsen-001@users.noreply.github.com>
2022-10-16 00:02:44 +0430

    Validation is done

--- app/signup-app.js
+++ app/signup-app.js
@@ -1,28 +1,103 @@
-const firstName = document.querySelector('.first-name input');
-const lastName = document.querySelector('.last-name input');
-const email = document.querySelector('.email input');
-const password = document.querySelector('.password input');
-const rePassword = document.querySelector('.re-password input');
+const signUp = document.querySelector('.submit .g-btn-primary');
+const haveAccount = document.querySelector('.submit .g-btn-primary');
+
+const form = document.querySelector('#form');
+const firstName = document.querySelector('#firstname');
+const lastName = document.querySelector('#lastname');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const rePassword = document.querySelector('#re-password');
 const policyAgreement = document.querySelector('.policy-agreement input');
 
 
-const signUp = document.querySelector('.submit .g-btn-primary');
-
-
-signUp.addEventListener('click', () => {
-    const user = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        password: password.value,
-        policyAgreement: policyAgreement.value
+function checkFirstname() {
+    const firstNameValue = firstName.value.trim();
+    if (firstNameValue === '') {
+        setError(firstName, 'First Name is required!');
+        return false;
+    }else{
+        return true;
     }
+}
 
-    const userObj = JSON.stringify(user);
-    localStorage.setItem('userInfo', userObj);
+function checkLastname() {
+    const lastNameValue = lastName.value.trim();
+    if (lastNameValue === '') {
+        setError(lastName, 'Last Name is required!');
+        return false;
+    }else{
+        return true;
+    }
+}
 
-    setTimeout(() => {
-        window.location.href = "./index.html";
-    }, 2000);
-    
+function checkEmail() {
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setError(email, 'Email is required!');
+        return false;
+    }else if (!validateEmail(emailValue)) {
+        setError(email, 'Please Enter a valide email address!');
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function checkPassword() {
+    const passwordValue = password.value.trim();
+    if (passwordValue === "") {
+        setError(password, 'Password is required!');
+        return false;
+    }else{
+        return true
+    }
+}
+
+function checkRePassword() {
+    const rePasswordValue = rePassword.value.trim();
+    const passwordValue = password.value.trim();
+    if (rePasswordValue === "") {
+        setError(rePassword, 'Repassword is required!');
+        return false;
+    }else if (passwordValue !== rePasswordValue){
+        setError(rePassword, 'Password doesnot match!');
+        return false;
+    }else{
+        return true
+    }
+}
+
+function setError(elem, msg) {
+    let formControl = elem.parentElement;
+    let small = formControl.querySelector('small');
+    formControl.classList.add('g-error');
+    small.innerText = msg;
+}
+
+signUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    // console.log('hey')
+    const firstNameValidated = checkFirstname();
+    const lastNameValidated = checkLastname();
+    const emailValidated = checkEmail();
+    const passwordValidated = checkPassword();
+    const rePasswordValidated = checkRePassword();
+
+    if(firstNameValidated && lastNameValidated && emailValidated && passwordValidated && rePasswordValidated){
+        setTimeout(() => {
+          window.location.href = "./index.html";
+        // console.log('info added')
+        }, 3000);
+    }
 })
+
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+// signUp.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     window.location.href = "./signup.html";
+// })
