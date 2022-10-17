e859e345f8163fe7c8f7ef27b6c515e8908da1f1
mohsen-001 <115645631+mohsen-001@users.noreply.github.com>
2022-10-17 10:09:32 +0430

    dashboard started in design

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+const sidebar_btn = document.querySelector('.fa-bars-staggered');
+const sidebar = document.querySelector('#sidebar');
+
 if (!localStorage.getItem('currentUser')) window.location.href = "./login.html";
 setTimeout(() => {
     let body = document.querySelector('body');
@@ -5,3 +8,8 @@ setTimeout(() => {
     loader.style.display = 'none';
     body.style.overflow = 'auto';
 }, 3000)
+
+
+sidebar_btn.addEventListener('click', () => {
+    sidebar.classList.toggle('expand-sidebar');
+});
